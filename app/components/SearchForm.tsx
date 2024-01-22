8e2a9da66fad93f4572cd8752494bc5a4d4d165d
no-pla <songjihyun323@gmail.com>
2024-01-22 15:45:20 +0900

    Design: add Search Form

---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+
+const SearchForm = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState(false);
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (inputRef.current?.value === "" || !inputRef.current) {
+      setError(true);
+      return;
+    }
+    console.log(inputRef.current.value);
+    // TODO: API 연동하기
+    setError(false);
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  return (
+    <form onSubmit={onSubmit} className="relative">
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="Search for any word..."
+        className={`w-full rounded-2xl bg-[#f4f4f4] px-6 py-5 border ${
+          error ? "border-[#ff5252]" : "border-transparent"
+        } focus:border-[#A445ed] outline-none
+        dark:bg-[#1f1f1f]
+        `}
+      />
+      {error && (
+        <p className="text-[#ff5252] text-headingS">
+          Whoops, can&#39;t be empty...
+        </p>
+      )}
+      <div className="bg-search w-4 h-4 absolute top-1/2 transform -translate-y-1/2 right-6" />
+    </form>
+  );
+};
+
+export default SearchForm;
